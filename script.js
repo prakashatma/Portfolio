14d8315dbcfc887562878fb76506fc8b8d344b43
unknown <8922894378ap@gmail.com>
2021-04-06 02:36:07 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,30 @@
+// navbar change color
+
+x = document.querySelector("nav");
+window.addEventListener("scroll" , ()=>{
+    if(window.scrollY){
+        x.classList.add("redcolor");        
+    }
+    else{
+        x.classList.remove("redcolor");
+    }
+})
+
+
+
+
+// typing animation script
+
+var typed = new Typed(".typing", {    
+    strings:["Frontend Developer." , "Fullstack Developer."],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+var typed = new Typed(".typings", {    
+    strings:[" Frontend Developer." , " Fullstack Developer."],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
